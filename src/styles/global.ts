9e05239d572182d97a29d3e3c6a8c41a1b68b08a
Dyeniffer Balçanelli <dyeni.dcb@gmail.com>
2023-10-02 10:59:39 -0300

    feat: input search

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,12 +6,20 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.blue};
+  }
+
   body {
     background: ${(props) => props.theme['base-background']};
     color: ${(props) => props.theme['base-text']};
     -webkit-font-smoothing: antialiased;
   }
+
   body, input, text-area, button {
     font: 400 1rem Nunito, sans-serif;
+    line-height: 160%;
   }
 `
