e029a49313c8396e31d8a70ad80dd9ff874a3c1d
Dyeniffer Balçanelli <dyeni.dcb@gmail.com>
2023-10-02 11:37:26 -0300

    feat: post page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Home } from './pages/Home'
+import { Post } from './pages/Post'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 import { DefaultLayout } from './layout/Default'
@@ -14,6 +15,8 @@ export function App() {
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
             <Route path="/" element={<Home />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
       </BrowserRouter>
