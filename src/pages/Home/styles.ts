fb495d99213e229c370b5778027ba3e262e4ce29
Dyeniffer Balçanelli <dyeni.dcb@gmail.com>
2023-10-02 13:57:39 -0300

    feat: search input

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -41,6 +41,16 @@ export const InputSearch = styled.input`
   }
 `
 
+export const NoContentContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > div {
+    width: auto !important;
+  }
+`
+
 export const CartsContainer = styled.div`
   margin-top: 3rem;
   display: grid;
